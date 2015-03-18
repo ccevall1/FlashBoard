7813f117b19b0a8d53100a525dae0669b2a9f4a7
ccevall1 <ccevall1@jhu.edu>
2015-03-18 01:59:34 -0400

    Update Quiz.java

--- FlashBoard/app/src/main/java/com/example/christian/flashboard/Quiz.java
+++ FlashBoard/app/src/main/java/com/example/christian/flashboard/Quiz.java
@@ -5,15 +5,23 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
 public class Quiz extends ActionBarActivity {
+
+    private GestureDetectorCompat mGestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        TextView mTextView = (TextView) findViewById(R.id.textView);
+        mGestureDetector = new GestureDetectorCompat(this, new MyGestureListener());
     }
-
+    
+    @Override 
+    public boolean onTouchEvent(MotionEvent event){ 
+        this.mGestureDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -28,12 +36,31 @@ public class Quiz extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+    
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+
+        //For changing cards
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2, 
+                float velocityX, float velocityY) {
+            //if x velocity < 0, fling left
+            
+            //if x velocity > 0, fling right
+            
+            return true;
+        }
+        
+        //For flipping the card
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            
+        }
+        
     }
 }
